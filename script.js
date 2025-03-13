698a251d6ec00c4c2ad4fd38caf7d9a6f1dd387c
leonardoGiaretta11 <leonardo.giaretta@edu.pucrs.br>
2025-03-13 15:39:09 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,64 @@
+let flag = 0;
+
+let flag_parenteses = 0;
+
+function show (ta) {
+
+    let num = ta - '0';
+
+    if (document.getElementById("number").innerHTML.length >= 16) 
+        return;
+
+    if (flag == 1) {
+        flag = 0;
+        document.getElementById("number").innerHTML = num;
+        return;
+    }
+
+    if (document.getElementById("number").innerHTML === "0") {
+        document.getElementById("number").innerHTML = ta;
+        return;
+    }
+
+    document.getElementById("number").innerHTML += num;
+}
+
+function clearAC () {
+    document.getElementById("number").innerHTML = "0"; 
+    flag_parenteses = 0;
+}
+
+function operat0(op) {
+    if (op === '.' || op === '%') {
+        document.getElementById("number").innerHTML += op;
+        return;
+    }
+    document.getElementById("number").innerHTML += ' ';
+    document.getElementById("number").innerHTML += op + ' ';
+}
+
+function myEvaluator () {
+    document.getElementById("number").innerHTML = eval(document.getElementById("number").innerHTML);
+    flag = 1;
+}
+
+function myPar() {
+    if (flag_parenteses == 1) {
+        document.getElementById("number").innerHTML = ( document.getElementById("number").innerHTML === '0') ?  document.getElementById("number").innerHTML = ')' :  document.getElementById("number").innerHTML += ')';
+        flag_parenteses = 0;
+        return;
+    }
+
+    document.getElementById("number").innerHTML = ( document.getElementById("number").innerHTML === '0') ?  document.getElementById("number").innerHTML = '(' :  document.getElementById("number").innerHTML += '(';
+    flag_parenteses = 1;
+    
+}
+
+function eraser(){
+    if (document.getElementById("number").innerHTML.length === 1) {
+        document.getElementById("number").innerHTML = 0;
+        return;
+    }
+    let str = document.getElementById("number").innerHTML;
+    document.getElementById("number").innerHTML = str.substring(0, str.length-1);
+}
